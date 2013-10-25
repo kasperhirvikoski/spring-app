160879f78306f6e41dc1c4cb73a6dca03188bf28
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-10-25 15:24:33 +0300

    Finalise field

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,11 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public final class App {
 
+    private static final ApplicationContext CONTEXT = new ClassPathXmlApplicationContext("spring-context.xml");
+
     private App() {}
 
     public static void main(String[] args) {
-
-        final ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 
         System.out.println("Hello World!");
     }
