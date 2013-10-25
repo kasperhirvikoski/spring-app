56c3fe10254f4c5a0c78fb46e8b7c4a81c07cf56
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-10-25 22:29:49 +0300

    Import assertations statically

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -5,13 +5,13 @@ package ${package};
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-context.xml")
@@ -32,6 +32,6 @@ public class AppTest {
     @Test
     public void youShallPass() {
 
-        Assert.assertTrue(true);
+        assertTrue(true);
     }
 }
